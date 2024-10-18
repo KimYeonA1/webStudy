be718148d9513fc0cd6255864a2906e742a0da91
KimYeonA1 <85yuna@naver.com>
2024-10-18 12:48:30 +0900

    index html 자바스크립트 기능추가, style 기능 수정

---
+++ html/chapter01/index_script.js
@@ -0,0 +1,19 @@
+// div#box객체를 가져온다.(스네이크 표기법만 씀)
+let div_box = window.document.getElementById("box");
+let position = 0;
+
+function moveBox(){
+    if(position < 200 ){
+        position += 1;
+        div_box.style.left = position + "px";
+    }else{
+        position = 0;
+        div_box.style.left = position + "px";
+    }
+    //계속해서 moveBox()를 불러주는 콜백기능을 부여한다
+    window.requestAnimationFrame(moveBox)
+}
+
+moveBox();
+
+
